faeecaf5965a8f39d6fbb7503425aedf26a7f675
Daniel Rodriguez <dfrodriguez_45@outlook.com>
2024-06-17 17:20:26 -0600

    refactor: Update npm dependencies and add @vercel/analytics package

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 
-import App from './App'
-import './index.css'
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-)
+    <App />
+    <Analytics />
+  </React.StrictMode>
+);
